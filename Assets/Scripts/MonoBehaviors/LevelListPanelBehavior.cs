71312b0ff5a4fea9d8ec82c1d35fec916c863821
Jayzon Ty <jayzon.ty@gmail.com>
2020-06-19 14:38:53 +0800

    Added a scene for level selection

---
+++ Assets/Scripts/MonoBehaviors/LevelListPanelBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProcessScheduling
+{
+    public class LevelListPanelBehavior : MonoBehaviour
+    {
+        public Transform ListContentTransform;
+
+        private void Start()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
