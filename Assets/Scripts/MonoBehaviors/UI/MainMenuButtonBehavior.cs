eee2d2e55b3b7faf9e34a9e0c5e497f06579ab5f
Jayzon Ty <jayzon.ty@gmail.com>
2021-01-31 11:26:27 +0800

    Added functionality for the main menu button in the level select scene

---
+++ Assets/Scripts/MonoBehaviors/UI/MainMenuButtonBehavior.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace ProcessScheduling
+{
+    public class MainMenuButtonBehavior : MonoBehaviour
+    {
+        /// <summary>
+        /// Reference to the button component
+        /// </summary>
+        private Button buttonComponent;
+
+        /// <summary>
+        /// Unity callback function called when the script
+        /// is created.
+        /// </summary>
+        private void Awake()
+        {
+            buttonComponent = GetComponent<Button>();
+        }
+
+        /// <summary>
+        /// Unity callback function called when the script
+        /// is enabled.
+        /// </summary>
+        private void OnEnable()
+        {
+            if (buttonComponent != null)
+            {
+                buttonComponent.onClick.AddListener(OnButtonClick);
+            }
+        }
+
+        /// <summary>
+        /// Unity callback function called when the script
+        /// is disabled.
+        /// </summary>
+        private void OnDisable()
+        {
+            if (buttonComponent != null)
+            {
+                buttonComponent.onClick.AddListener(OnButtonClick);
+            }
+        }
+
+        /// <summary>
+        /// Handler function for when the button was clicked.
+        /// </summary>
+        private void OnButtonClick()
+        {
+            SceneManager.LoadScene("TitleScene");
+        }
+    }
+}
