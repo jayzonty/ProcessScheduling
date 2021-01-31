5b7695953f1afa9181f377be77ef8f4731d233b4
Jayzon Ty <jayzon.ty@gmail.com>
2021-01-31 19:10:09 +0800

    Added a scene that contains instructions on how to play the game

---
+++ Assets/Scripts/MonoBehaviors/UI/InstructionsButtonBehavior.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace ProcessScheduling
+{
+    /// <summary>
+    /// Script containing data and behavior for the instructions button
+    /// </summary>
+    public class InstructionsButtonBehavior : MonoBehaviour
+    {
+        /// <summary>
+        /// Reference to the button component script
+        /// </summary>
+        private Button buttonComponent;
+
+        /// <summary>
+        /// Unity callback function called when the script
+        /// is created.
+        /// </summary>
+        private void Awake()
+        {
+            buttonComponent = GetComponent<Button>();
+        }
+
+        /// <summary>
+        /// Unity callback function called when the script
+        /// is enabled.
+        /// </summary>
+        private void OnEnable()
+        {
+            if (buttonComponent != null)
+            {
+                buttonComponent.onClick.AddListener(OnButtonClicked);
+            }
+        }
+
+        /// <summary>
+        /// Unity callback function called when the script
+        /// is disabled.
+        /// </summary>
+        private void OnDisable()
+        {
+            if (buttonComponent != null)
+            {
+                buttonComponent.onClick.RemoveListener(OnButtonClicked);
+            }
+        }
+
+        /// <summary>
+        /// Handler function for when the button was clicked
+        /// </summary>
+        private void OnButtonClicked()
+        {
+            SceneManager.LoadScene("InstructionsScene");
+        }
+    }
+}
